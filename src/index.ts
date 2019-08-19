ddb4378a65fa2be496d032166f3ceb07e9aade31
krzykaczu <krzywiec@gmail.com>
2019-08-19 16:53:01 +0200

    Add cors and new ver of body-parser config

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,13 @@ import "dotenv/config";
 import * as express from "express";
 import { Request, Response } from "express";
 import config from "./config/config";
+import * as cors from "cors";
 
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
   res.send({
