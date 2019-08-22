bc6cd337b82fef695a9fca0e31a11c2dccf5bfb1
krzykaczu <krzywiec@gmail.com>
2019-08-22 17:16:06 +0200

    Add error handlers - mind - static path to views due to webpack - to be sorted

--- src/index.ts
+++ src/index.ts
@@ -5,55 +5,56 @@ import * as cors from "cors";
 import * as mongoose from "mongoose";
 import routes from "./routes";
 import models from "./models";
+import { handle404, catchErrors } from "./middleware/errorHandlers";
+import { join } from "path";
 
 //checking db
-const createTechnologyWithSnippets = async () => {
-  const tech1 = new models.Technology({
-    name: "javascript"
-  });
+// const createTechnologyWithSnippets = async () => {
+//   const tech1 = new models.Technology({
+//     name: "javascript"
+//   });
 
-  const tech2 = new models.Technology({
-    name: "Visual studio"
-  });
+//   const tech2 = new models.Technology({
+//     name: "Visual studio"
+//   });
 
-  const snippet1 = new models.Snippet({
-    code: "this is a snippet with event delegation",
-    description: "event delegation",
-    technology: tech1.id
-  });
+//   const snippet1 = new models.Snippet({
+//     code: "this is a snippet with event delegation",
+//     description: "event delegation",
+//     technology: tech1.id
+//   });
 
-  const snippet2 = new models.Snippet({
-    code: "copy line snippet",
-    description: "copy line",
-    technology: tech2.id
-  });
+//   const snippet2 = new models.Snippet({
+//     code: "copy line snippet",
+//     description: "copy line",
+//     technology: tech2.id
+//   });
 
-  const snippet3 = new models.Snippet({
-    code: "comment line snippet",
-    description: "comment line",
-    technology: tech2.id
-  });
+//   const snippet3 = new models.Snippet({
+//     code: "comment line snippet",
+//     description: "comment line",
+//     technology: tech2.id
+//   });
 
-  await snippet1.save();
-  await snippet2.save();
-  await snippet3.save();
+//   await snippet1.save();
+//   await snippet2.save();
+//   await snippet3.save();
 
-  await tech1.save();
-  await tech2.save();
+//   await tech1.save();
+//   await tech2.save();
 
-  // await tech2.remove();
-};
+//   // await tech2.remove();
+// };
 // end checking db
 
-mongoose
-  .connect(config.database, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useFindAndModify: false
-  })
-  .then(async () => {
-    createTechnologyWithSnippets();
-  });
+mongoose.connect(config.database, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+// .then(async () => {
+//   createTechnologyWithSnippets();
+// });
 
 mongoose.connection.on("error", err => {
   console.log("Could not connect to the database. Exiting now...");
@@ -62,11 +63,17 @@ mongoose.connection.on("error", err => {
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", "./src/views");
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
 app.use("/api", routes());
+
+app.use(handle404);
+app.use(catchErrors);
 
 if (require.main === module) {
   app.listen(config.server.port, () => {
