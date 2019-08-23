dadc3ec77400a0e2608ca227f1da9bcb108d4bc5
krzykaczu <krzywiec@gmail.com>
2019-08-23 19:14:38 +0200

    Add REST DELETE snippet

--- src/controllers/snippetsController.ts
+++ src/controllers/snippetsController.ts
@@ -1,3 +1,52 @@
+import Technology from "../models/technology";
 import Snippet from "../models/snippet";
+import { Request, Response, NextFunction } from "express";
 
-export default {};
+export default {
+  async findOneByTech(req: Request, res: Response, next: NextFunction) {
+    const technology = await Technology.findOne({ slug: req.params.slug1 });
+    const snippetByTechnologyAndSlug = await Snippet.find({
+      technology: technology._id,
+      slug: req.params.slug2
+    });
+    if (!technology) return next();
+
+    return res.status(200).send({ data: { snippetByTechnologyAndSlug } });
+  },
+  async create(req: Request, res: Response) {
+    //2do - validate prior to saving
+    const technology = await Technology.findOne({ slug: req.params.slug });
+    const snippet = await new Snippet({
+      code: req.body.code,
+      description: req.body.description,
+      technology: technology.id
+    }).save();
+
+    return res.status(201).send({ data: snippet, message: "Snippet created" });
+  },
+  async update(req: Request, res: Response, next: NextFunction) {
+    //2do - validate prior to saving
+    const technology = await Technology.findOne({ slug: req.params.slug1 });
+    const snippet = await Snippet.findOneAndUpdate(
+      { slug: req.params.slug2 },
+      {
+        code: req.body.code,
+        description: req.body.description,
+        technology: req.body.technology ? req.body.technology : technology.id
+      },
+      { new: true }
+    );
+    if (!snippet) return next();
+
+    return res
+      .status(200)
+      .send({ data: snippet, message: "Snippet was updated" });
+  },
+  async delete(req: Request, res: Response, next: NextFunction) {
+    const snippet = await Snippet.findOneAndDelete({
+      slug: req.params.slug2
+    });
+    if (!snippet) return next();
+    return res.status(200).send({ message: "Snippet deleted" });
+  }
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import technologyController from "../controllers/technologyController";
+import snippetsController from "../controllers/snippetsController";
 import { catchAsyncDecorator } from "../middleware/errorHandlers";
 
 const routes = () => {
@@ -14,6 +15,14 @@ const routes = () => {
   api.put("/:slug", catchAsyncDecorator(technologyController.update));
   //DELETE
   api.delete("/:slug", catchAsyncDecorator(technologyController.delete));
+
+  api.get(
+    "/:slug1/:slug2",
+    catchAsyncDecorator(snippetsController.findOneByTech)
+  );
+  api.post("/:slug", catchAsyncDecorator(snippetsController.create));
+  api.put("/:slug1/:slug2", catchAsyncDecorator(snippetsController.update));
+  api.delete("/:slug1/:slug2", catchAsyncDecorator(snippetsController.delete));
 
   return api;
 };
